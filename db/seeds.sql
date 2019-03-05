17391c7bb0521e15930fe812d2c2116d6a6e858a
MinhTran0220 <minhngocny@hotmail.com>
2019-03-05 02:19:41 -0500

    OK

---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+-- Select the burgers_db
+USE burgers_db;
+
+-- Insert new rows of data.
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("hamburger", false);  
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("cheeseburger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Fish burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("BigMac burger", false);
+
+
+
